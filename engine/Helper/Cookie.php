a522f36d9ea995ed62a5e50dec743a08bf2f0233
Puffic Man <rezaevigor@yandex.ru>
2025-04-18 14:45:15 +0300

    Create class COOKIE

---
+++ engine/Helper/Cookie.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Engine\Helper;
+
+class Coockie
+{
+    public static function set($key, $value, $time = 31536000)
+    {
+        return setcookie($key, $value, time() +  $time, '/');
+    }
+
+    public static function get($key)
+    {
+        if (isset($_COOKIE[$key])) {
+            return $_COOKIE[$key];
+        }
+
+        return null;
+    }
+
+    public function delete($key)
+    {
+        if ($_COOKIE[$key]) {
+            self::set($_COOKIE[$key], '', -3600);
+            unset($_COOKIE[$key]);
+        }
+    }
+}
+
