c564d555e00bef461e2c4a44989792a6c430e61a
Puffic Man <rezaevigor@yandex.ru>
2025-04-21 13:24:59 +0300

    fix code conection

--- engine/Core/Database/Connection.php
+++ engine/Core/Database/Connection.php
@@ -20,34 +20,23 @@ class Connection
     private function connect() 
     {
         $config = Config::file('database');
-
-        echo "<pre>";
-        print_r($config);
-        echo "</pre>";
-
         $dsn = "mysql:host=".$config['host'].";dbname=".$config['db_name'].";charset=".$config['charset'];
         $this->link = new PDO($dsn, $config['username'], $config['password']);
-
         return $this;
     }
 
     public function execute($sql)
     {
         $sth = $this->link->prepare($sql);
-
         return $sth->execute();
     }
 
     public function query($sql)
     {
 
-        print_r($this->link);
         $sth = $this->link->prepare($sql);
-
         $sth->execute();
-
         $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-
         if ($result === false) {
             return [];
         }
