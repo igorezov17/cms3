548117872387231edf7a77a4e620b44216e028a9
Igor small black <rezaevigor@yandex.ru>
2025-11-23 21:16:47 +0300

    added new methods

--- cms/Controller/OrderController.php
+++ cms/Controller/OrderController.php
@@ -6,4 +6,19 @@ class OrderController
         public string $name;
     )
     {}
+
+    public function setName(string $name):void
+    {
+        $thid->name = $name;
+    }
+
+    public function getName():OrderController
+    {
+        return $this->name;
+    }
+
+    private function has()
+    {
+        return $this->name ?? null;
+    }
 }
